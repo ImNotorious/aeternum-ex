e9d7fec2f3f3b4a88ad127cdb30f3b168ab12630
ImNotorious <jain05akshat@gmail.com>
2025-03-29 16:43:31 +0530

    added new fetch call

--- client/app/analysis/heart-disease/page.tsx
+++ client/app/analysis/heart-disease/page.tsx
@@ -78,7 +78,7 @@ export default function HeartDiseasePage() {
     console.log('Request Data:', JSON.stringify(requestData, null, 2));
   
     try {
-      const response = await fetch('http://localhost:5000/predict', {
+      const response = await fetch('https://heart-ml-api-bkl0.onrender.com/predict', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
