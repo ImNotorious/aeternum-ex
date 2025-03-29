d7ab73aed9628bd15261ed321031c00ca9940c80
ImNotorious <jain05akshat@gmail.com>
2025-03-29 16:26:03 +0530

    fixing trying

--- server/model_1/app.py
+++ server/model_1/app.py
@@ -45,4 +45,5 @@ def predict_heart_disease():
     })
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
